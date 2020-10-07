93c9bea89e26d9c706f20d276d6e363f3eb6debd
GKO95 <44629526+GKO95@users.noreply.github.com>
2020-10-07 17:56:18 +0900

    Attempting Win32.EDID with Python

---
+++ EDID.py/main.py
@@ -0,0 +1,14 @@
+from ctypes import *
+
+print(windll.setupapi)
+
+class SetupAPI:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def SetupDiClassGuidsFromNameW(ClassName, ClassGuidList, ClassGuidListSize, RequiredSize):
+        pass
+
+if __name__ == "__main__":
+    pass
